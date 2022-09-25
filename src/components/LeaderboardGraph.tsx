33f2aa4897dc6cba4b5117fae1caf541dce836d6
Jonah Wiecek <jonahwiecek@gmail.com>
2022-09-25 10:50:49 +0200

    #13 feat: added Leaderboard graph component
    Related to #6

---
+++ src/components/LeaderboardGraph.tsx
@@ -0,0 +1,56 @@
+import { ReactComponent as First } from "../assets/medals/1place-medal.svg";
+import { ReactComponent as Second } from "../assets/medals/2place-medal.svg";
+import { ReactComponent as Third } from "../assets/medals/3place-medal.svg";
+
+export interface LeaderboardGraphProps {
+  category: string;
+  winners: Array<Winner>;
+}
+export interface Winner {
+  imageUrl?: string;
+  name: string;
+  value: number;
+}
+
+export const LeaderboardGraph = ({
+  category,
+  winners,
+}: LeaderboardGraphProps) => {
+  //sort the winners by value
+  winners.sort((a, b) => b.value - a.value);
+  const first = winners[0];
+  winners[0] = winners[1];
+  winners[1] = first;
+
+  const MAX_BAR_BONUS_HEIGHT = 200;
+  const MIN_BAR_HEIGHT = 130;
+  const data = winners.map((winner, key) => {
+    const imgPath = "../assets/medals/" + key + "place-medal.svg";
+    console.log(imgPath);
+    return (
+      <div className="leaderboard winner">
+        <img
+          className="leaderboard image"
+          src={winner.imageUrl || "https://picsum.photos/200"}
+          alt="winner"
+        />
+        <div className="leaderboard name">{winner.name}</div>
+        <div className="leaderboard score">{winner.value + " " + category}</div>
+        <div
+          className="podium"
+          style={{
+            height:
+              MIN_BAR_HEIGHT +
+              (MAX_BAR_BONUS_HEIGHT * winner.value) / first.value,
+          }}
+        >
+          {key === 0 && <Second />}
+          {key === 1 && <First />}
+          {key === 2 && <Third />}
+        </div>
+      </div>
+    );
+  });
+
+  return <div className="leaderboard container">{data}</div>;
+};
